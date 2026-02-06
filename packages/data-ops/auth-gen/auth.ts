6e1821c129a9cf12f447595322099bc3cc2ca4cf
Josh <jxsh@hey.com>
2026-02-06 15:06:36 +0530

    added auth

--- packages/data-ops/auth-gen/auth.ts
+++ packages/data-ops/auth-gen/auth.ts
@@ -1,6 +1,11 @@
-import { betterAuth } from "better-auth";
-import Database from "better-sqlite3";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createBetterAuth } from "@/auth";
 
-export const auth = betterAuth({
-  database: new Database("./sqlite.db"),
-});
+export const auth: ReturnType<typeof createBetterAuth> = createBetterAuth(
+	drizzleAdapter(
+		{},
+		{
+			provider: "sqlite",
+		},
+	),
+);
